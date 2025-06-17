db5bcb19a000904f9e7aa67063ec85fa0cde3690
Piyush <pk9995114@gmail.com>
2025-06-17 23:15:52 +0530

    nothing

--- frontend/src/pages/FoundPage.jsx
+++ frontend/src/pages/FoundPage.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import '../styles/FoundPage.css'
+
 import Navbar from '../components/Navbar'
 import FoundDashboard from '../components/FoundDashboard'
 function FoundPage() {
